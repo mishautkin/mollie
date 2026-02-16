c228884984cdeb7f695e2f662034da253663596d
Misha Utkin <m.utkin@inpsyde.com>
2026-02-16 18:11:22 +0100

    Update

--- tests/qa/playwright.config.ts
+++ tests/qa/playwright.config.ts
@@ -35,7 +35,17 @@ export default defineConfig< TestExtend >({
     baseURL: process.env.WP_BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
+
+		screenshot: {
+			mode: 'only-on-failure',
+			fullPage: true, // Captures entire scrollable page
+		},
+
+		video: {
+			mode: 'retain-on-failure', //'on',//
+			size: { width: 1280, height: 850 },
+		},
   },
 
   /* Configure projects for major browsers */
