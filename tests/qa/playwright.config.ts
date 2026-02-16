d50df5d086db6c638c7cd11e8ea8d1583386a18f
Misha Utkin <m.utkin@inpsyde.com>
2026-02-16 12:00:46 +0100

    Correction

--- tests/qa/playwright.config.ts
+++ tests/qa/playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig< TestExtend >({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
+      name: 'all',
       use: { ...devices['Desktop Chrome'] },
     },
 
